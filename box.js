9f2eb8d90186164ca87719896fe810738fddbc1f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-04-08 15:54:57 -0600

    refactor(box): introduce consequent execution

--- box.js
+++ box.js
@@ -1,9 +1,17 @@
-const nextCharForNumberString = str => {
-    const trimmed = str.trim();
-    const number = parseInt(trimmed);
-    const nextNumber = number + 1;
+// const nextCharForNumberString = str => {
+//     const trimmed = str.trim();
+//     const number = parseInt(trimmed);
+//     const nextNumber = number + 1;
+//
+//     return String.fromCharCode(nextNumber);
+// }
 
-    return String.fromCharCode(nextNumber);
+const nextCharForNumberString = str => {
+    return [str]
+        .map(s => s.trim())
+        .map(s => parseInt(s))
+        .map(n => n + 1)
+        .map(n => String.fromCharCode(n))
 }
 
 const result = nextCharForNumberString(" 64 ");
