d2b276ab354af2fe597f944563ceed4bcaf1d278
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-04-10 18:37:24 -0600

    refactor(box): refactor apply discount

--- box.js
+++ box.js
@@ -15,18 +15,15 @@ let result = nextCharForNumberString(" 64 ");
 
 
 const moneyToFloat = str => {
-    return Box(str).map(s => s.replace(/[$,]/g, '')).fold(s => parseFloat(s));
+    return Box(str.replace(/[$,]/g, '')).map(s => parseFloat(s));
 };
 
 const percentToFloat = str => {
-    return Box(str).map(s => s.replace(/[$,]/g, '')).map(s => parseFloat(s)).fold(n => n / 100)
+    return Box(str.replace(/[$,]/g, '')).map(s => parseFloat(s)).map(n => n / 100)
 };
 
 const applyDiscount = (price, discount) => {
-    const cost = moneyToFloat(price);
-    const savings = percentToFloat(discount);
-
-    return cost - (cost * savings);
+    return moneyToFloat(price).fold(cost => percentToFloat(discount).fold(savings => cost - (cost * savings)))
 }
 
 result = applyDiscount("$100.00", "20%");
