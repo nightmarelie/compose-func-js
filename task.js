787c85f365d3bd72a87275ba0f73cc10d1a2345e
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-05 11:30:04 -0600

    feat(task): add task example

--- task.js
+++ task.js
@@ -1,1 +1,11 @@
 const Task = require('data.task');
+
+const launchMissiles = () => {
+    return new Task((rej, res) => {
+        setTimeout(() => {
+        })
+        res('missiles launched');
+    }, 1000);
+};
+
+launchMissiles().fork(e => console.log('error', e), x => console.log('success', x));
