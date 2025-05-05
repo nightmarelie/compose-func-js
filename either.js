204596724f678d45fece8f509080793b5ed28ca3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-04 18:05:38 -0600

    feat(either): functional impl

--- either.js
+++ either.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 
 const Right = x => ({
-    map: f => Right(f(x)),
-    fold: (_, g) => g(x),
+    map: f => Right(f(x)), fold: (_, g) => g(x),
 })
 
 const Left = x => ({
-    map: f => Left(x),
-    fold: (f, _) => f(x),
+    map: f => Left(x), fold: (f, _) => f(x),
 })
 
 const Either = Right || Left;
@@ -20,13 +18,8 @@ const fromNullable = x => {
 
 const findColor = (color) => {
     const found = ({
-            red: '#FF0000',
-            green:
-                '#00FF00',
-            blue:
-                '#0000FF',
-        }
-    )[color];
+        red: '#FF0000', green: '#00FF00', blue: '#0000FF',
+    })[color];
 
     return fromNullable(found);
 };
@@ -34,24 +27,27 @@ const findColor = (color) => {
 // console.log(findColor('black').map(x => x.slice(1)).fold(x => x, x => x.toUpperCase()));
 
 const tryCatch = (fn) => {
-    return (...args) => {
-        try {
-            return Right(fn(...args));
-        } catch (e) {
-            return Left(e);
-        }
+    try {
+        return Right(fn());
+    } catch (e) {
+        return Left(e);
     }
 }
+
+// const getPort = () => {
+//     try {
+//         const str = fs.readFileSync('config.json');
+//
+//         const config = JSON.parse(str);
+//         return config.port;
+//     } catch (e) {
+//         return 3000;
+//     }
+// }
 
 const getPort = () => {
-    try {
-        const str = fs.readFileSync('config.json');
-
-        const config = JSON.parse(str);
-        return config.port;
-    } catch (e) {
-        return 3000;
-    }
+    return tryCatch(() => fs.readFileSync('config.json')).map(str => JSON.parse(str)).map(config => config.port).fold(() => 3000, x => x);
 }
+
 
 console.log(getPort());
