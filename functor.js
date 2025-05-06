0d3a217ff351dd234c60a42df9b734c04f9ab9b4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-06 15:56:23 -0600

    feat(functor): Lift into a pointed functor

--- functor.js
+++ functor.js
@@ -1,2 +1,13 @@
 // First law
 // fx.map(f).map(g) === fx.map(x => g(f(x)));
+
+ // Second law
+// fx.map(f) === f(x)
+
+const Box = require('./box');
+const Either = require('./either');
+const Task = require('data.task');
+
+Task.of('Hello World'); // Task('Hello World')
+Either.of('Hello World'); // Right('Hello World')
+Box.of('Hello World'); // Box('Hello World')
