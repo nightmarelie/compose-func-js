9012906115d1bc92387657a841d4d40f1989aac4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-05-06 15:38:58 -0600

    feat(task): create app func

--- task.js
+++ task.js
@@ -1,4 +1,5 @@
 const Task = require('data.task');
+const fs = require('fs');
 
 const launchMissiles = () => {
     return new Task((rej, res) => {
@@ -8,4 +9,22 @@ const launchMissiles = () => {
     }, 1000);
 };
 
-launchMissiles().fork(e => console.log('error', e), x => console.log('success', x));
+const app = () => {
+    fs.readFile('config.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        const newData = data.replace(/8/g, '6');
+
+        fs.writeFile('config.json', newData, (err) => {
+            if (err) {
+                throw err;
+            }
+
+            console.log('File updated successfully');
+        });
+    });
+}
+
+app();
